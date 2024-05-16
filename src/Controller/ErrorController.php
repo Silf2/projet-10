fd164d1606e9b936df9dd61ddf2acf5fbdcbca7c
Silf2 <sylvain.guenet1@gmail.com>
2024-05-16 15:43:35 +0200

    Ajout d'une page d'erreur

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ErrorController extends AbstractController
+{
+    #[Route('/error', name: 'app_error')]
+    public function showError(\Throwable $exception): Response
+    {
+        if ($exception instanceof HttpExceptionInterface) {
+            $statusCode = $exception->getStatusCode();
+            $message = $exception->getMessage();
+            return $this->render('error/error.html.twig', [
+                'errorCode' => $statusCode,
+                'errorMessage' => $message,
+            ]);
+        } else {
+            $statusCode = 500;
+            $message = $exception->getMessage();
+            return $this->render('error/error.html.twig', [
+                'errorCode' => $statusCode,
+                'errorMessage' => $message,
+            ]);
+        }
+    }
+}
